e528746278c2370b51bc1b86a0063929b439223c
Olena Kostiuk <helena.kostyuk@gmail.com>
2020-02-15 18:38:50 +0200

    Добавьте файлы проекта.

---
+++ Tamagochi2/MainWindow.xaml.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Tamagochi2
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Tamagochi tamagochi;
+        private readonly System.Windows.Threading.DispatcherTimer timerLife = new System.Windows.Threading.DispatcherTimer();
+        private readonly System.Windows.Threading.DispatcherTimer timerThoughts = new System.Windows.Threading.DispatcherTimer();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnStartGame_Click(object sender, RoutedEventArgs e)
+        {
+            tamagochi = new Tamagochi();
+
+            DrawGame();
+            DrawTamagochi();
+
+            DrawLifeBarDecreasing();
+            DrawThoughtsChange();
+
+
+        }
+
+        private void DrawThoughtsChange()
+        {
+            timerThoughts.Interval = TimeSpan.FromSeconds(10);
+            timerThoughts.Tick += TickThought;
+            timerThoughts.Start();
+        }
+
+        private void TickThought(object sender, EventArgs e)
+        {
+            tamagochi.Thought = tamagochi.GetNewThought();
+            DrawTamagochiThougts();
+        }
+
+        private void DrawLifeBarDecreasing()
+        {
+            timerLife.Interval = TimeSpan.FromSeconds(1);
+            timerLife.Tick += TickLife;
+            timerLife.Start();
+        }
+
+        private void TickLife(object sender, EventArgs e)
+        {
+            tamagochi.LowerLife(10);
+            DrawTamagochiLifeBar();
+        }
+
+        private void DrawTamagochi()
+        {
+            DrawTamagochiLifeBar();
+            DrawTamagochiThougts();
+        }
+
+        private void DrawTamagochiThougts()
+        {
+            myCanvasThoughts.Children.Clear();
+            Shape thoughts = tamagochi.GetThoughtsShape();
+            Canvas.SetLeft(thoughts, 80);
+            Canvas.SetTop(thoughts, 30);
+            myCanvasThoughts.Children.Add(thoughts);
+
+            Label lbThoughts = new Label();
+            lbThoughts.Content = tamagochi.Thought;
+            lbThoughts.Foreground = new SolidColorBrush(Colors.Green);
+            lbThoughts.FontWeight = FontWeights.Bold;
+            lbThoughts.FontSize = 15;
+
+            Canvas.SetLeft(lbThoughts, 100);
+            Canvas.SetTop(lbThoughts, -25);
+            myCanvasThoughts.Children.Add(lbThoughts);
+        }
+
+        private void DrawTamagochiLifeBar()
+        {
+            myCanvasLife.Children.Clear();
+            Shape lifeBar = tamagochi.GetLifeBar();
+            Canvas.SetLeft(lifeBar, 30);
+            Canvas.SetTop(lifeBar, 15);
+
+            myCanvasLife.Children.Add(lifeBar); 
+        }
+
+        private void DrawGame()
+        {
+            gridGame.Visibility = Visibility.Visible;
+        }
+
+        private void btnFeed_Click(object sender, RoutedEventArgs e)
+        {
+            if (tamagochi.Thought == tamagochi.Thoughts[0])
+            {
+                tamagochi.AddLife(tamagochi.LifeAdditionStep);
+                DrawTamagochiThougts();
+                DrawTamagochiLifeBar();
+            }
+
+        }
+        private void btnWalk_Click(object sender, RoutedEventArgs e)
+        {
+            if (tamagochi.Thought == tamagochi.Thoughts[1])
+            {
+                tamagochi.AddLife(tamagochi.LifeAdditionStep);
+                DrawTamagochiThougts();
+                DrawTamagochiLifeBar();
+            }
+        }
+        private void btnCure_Click(object sender, RoutedEventArgs e)
+        {
+            if (tamagochi.Thought == tamagochi.Thoughts[3])
+            {
+                tamagochi.AddLife(tamagochi.LifeAdditionStep);
+                DrawTamagochiThougts();
+                DrawTamagochiLifeBar();
+            }
+        }
+        private void btnSleep_Click(object sender, RoutedEventArgs e)
+        {
+            if (tamagochi.Thought == tamagochi.Thoughts[2])
+            {
+                tamagochi.AddLife(tamagochi.LifeAdditionStep);
+                DrawTamagochiThougts();
+                DrawTamagochiLifeBar();
+            }
+        }
+
+        private void btnPlay_Click(object sender, RoutedEventArgs e)
+        {
+            if (tamagochi.Thought == tamagochi.Thoughts[4])
+            {
+                tamagochi.AddLife(tamagochi.LifeAdditionStep);
+                DrawTamagochiThougts();
+                DrawTamagochiLifeBar();
+            }
+        }
+
+    }
+}
---
+++ Tamagochi2/Tamagochi.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Shapes;
+using System.Windows.Media;
+
+namespace Tamagochi2
+{
+    class Tamagochi
+    {
+        public int Life { get; set; }
+        public int LifeAdditionStep { get; set; }
+        public string Thought { get; set; }
+        public string[] Thoughts { get; set; }
+        public Tamagochi()
+        {
+            Life = 1000;
+            LifeAdditionStep = 150;
+            Thoughts = new string[] { "Feed me! ", "Walk with me!", "I want sleep!", "I am sick...", "I want to play!", "Thanks, Vika!" };
+            Thought = GetNewThought();
+        }
+
+        
+        public Shape GetLifeBar()
+        {
+            Rectangle lifeBar = new Rectangle();
+            lifeBar.Width = Life *2 /10;
+            lifeBar.Height = 20;
+            LinearGradientBrush filler = new LinearGradientBrush(Colors.Red, Colors.IndianRed, 1);
+            lifeBar.Fill = filler;
+            
+            return lifeBar;
+        }
+
+        internal Shape GetThoughtsShape()
+        {
+            Polygon thoughts = new Polygon();
+            SolidColorBrush filler = new SolidColorBrush();
+            filler.Color = Colors.FloralWhite;
+            thoughts.Fill = filler;
+
+            PointCollection points = new PointCollection();
+            points.Add(new Point(0, 0));
+            points.Add(new Point(15, -80));
+            points.Add(new Point(140, -50));
+            points.Add(new Point(130, 0));
+            points.Add(new Point(15, -20));
+
+            thoughts.Points = points;
+            SolidColorBrush stroke = new SolidColorBrush();
+            stroke.Color = Colors.Green;
+            thoughts.Stroke = stroke;
+            thoughts.StrokeThickness = 3;
+
+            return thoughts;
+        }
+
+        internal string GetNewThought()
+        {
+            Random rnd = new Random();
+            string thought = Thoughts[rnd.Next(0, 5)];
+            return thought;
+        }
+
+        internal void LowerLife(int lowlife)
+        {
+            if (Life <= lowlife)
+            { }
+            else
+            {
+                Life -= lowlife;
+            }
+        }
+
+        internal void AddLife(int addLife)
+        {
+            if (Life >= 2000)
+            { }
+            else
+            {
+                Life += addLife;
+            }
+
+            Thought = Thoughts[5];
+        }
+    }
+}
